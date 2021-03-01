3bc1f584d4f0871d6c18fcc373d42e0f0a0d2c72
Ohrannick <ohrannick@gmail.com>
2021-03-01 15:36:43 +0300

    change (optimization) main. js

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const PRODUCTS = [
     { id: 5, title: 'Аксессуары', price: 100, img: 'Accessories' }
 ];
 //Функция для формирования верстки каждого товара
-const renderProduct = (item) => {
-    return `<div id=${item.id} class="product-item">
-                <h3>${item.title}</h3>
-                <img class="card-img" src="img/${item.img}.jpeg" alt="${item.img}">
-                <p>Цена: ${item.price} &#36;</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
+const renderProduct = item =>
+    `<div id=${item.id} class="product-item">
+        <h3>${item.title}</h3>
+        <img class="card-img" src="img/${item.img}.jpeg" alt="${item.img}">
+        <p>Цена: ${item.price} &#36;</p>
+        <button class="buy-btn">Купить</button>
+    </div>`
+
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item));
     document.querySelector('.products').innerHTML = productsList.join("");
